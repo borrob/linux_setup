0f76917aafe1495a08d6acd4248547938b9a15d8
Rob van Loon <borrob@me.com>
2017-01-30 20:35:25 +0100

    Fix apt-get command

--- extra.sh
+++ extra.sh
@@ -3,5 +3,5 @@
 apt-get install vim sudo git locate curl sysv-rc-conf
 
 #Needed for Vbox guest additions
-apt get-install build-essential module-assistant
+apt-get install build-essential module-assistant
 m-a prepare
